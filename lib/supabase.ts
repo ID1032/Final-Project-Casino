0ed3e43dd2e69692c4c5c181f613ceec8ade0728
Taime <prayfah.pool@kmutt.ac.th>
2025-11-07 22:03:25 +0700

    edit API router of supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,11 +1,19 @@
-import { createClient } from '@supabase/supabase-js';
+import { cookies } from 'next/headers';
+import { createBrowserClient, createServerClient } from '@supabase/ssr';
+import type { SupabaseClient } from '@supabase/supabase-js';
 
-export const supabaseBrowser = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
-);
+export const createSupabaseBrowserClient = (): SupabaseClient => {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+};
 
-export const supabaseServer = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-);
+export const createSupabaseServerClient = (): SupabaseClient => {
+  const cookieStore = cookies();
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    { cookies: { get: name => cookieStore.get(name)?.value } }
+  );
+};
